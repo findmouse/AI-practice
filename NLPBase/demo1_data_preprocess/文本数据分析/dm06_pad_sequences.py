aa25e6b614ab66e74a5fa91b1d5772faa8e724db
dinghao <dinghao0513@gmail.com>
2026-06-13 20:27:24 +0900

    feat: テキスト長統一機能の実装

---
+++ NLPBase/demo1_data_preprocess/文本数据分析/dm06_pad_sequences.py
@@ -0,0 +1,20 @@
+from keras.preprocessing import sequence
+
+cutlen = 10
+
+
+def padding(x_train):
+    """
+    入力テキストテンソルの長さを統一する。
+
+    :param x_train: テキストのテンソル表現。形式: [[1, 32, 32, 61], [2, 54, 21, 7, 19]]
+    :return: パディングおよび切り捨て処理後のテキストテンソル表現
+    """
+    # sequence.pad_sequences を使用して長さを統一
+    return sequence.pad_sequences(x_train, cutlen, padding="pre", truncating="post")
+
+
+if __name__ == '__main__':
+    data = [[1, 23, 5, 32, 55, 63, 2, 21, 78, 32, 23, 1], [2, 32, 1, 23, 1]]
+    my_data = padding(data)
+    print(my_data)
